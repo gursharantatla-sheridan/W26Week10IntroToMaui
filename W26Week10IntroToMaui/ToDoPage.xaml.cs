5194f7a5fd48e4b83227338d51d131d7a00ae678
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-03-16 14:19:06 -0400

    to do example

--- W26Week10IntroToMaui/ToDoPage.xaml.cs
+++ W26Week10IntroToMaui/ToDoPage.xaml.cs
@@ -1,14 +1,24 @@
+using System.Collections.ObjectModel;
+
 namespace W26Week10IntroToMaui;
 
 public partial class ToDoPage : ContentPage
 {
+	ObservableCollection<string> tasks = new ObservableCollection<string>();
+
 	public ToDoPage()
 	{
 		InitializeComponent();
+		lvTasks.ItemsSource = tasks;
 	}
 
     private void OnAddTaskClicked(object sender, EventArgs e)
     {
-
+		if (!string.IsNullOrWhiteSpace(txtTask.Text))
+		{
+			tasks.Add(txtTask.Text);
+			txtTask.Text = string.Empty;
+			txtTask.Focus();
+		}
     }
 }
