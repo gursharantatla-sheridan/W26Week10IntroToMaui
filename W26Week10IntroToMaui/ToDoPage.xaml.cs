68c692d407638b39b29bbc450506b9f09e271166
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-03-16 14:10:36 -0400

    to do app

---
+++ W26Week10IntroToMaui/ToDoPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace W26Week10IntroToMaui;
+
+public partial class ToDoPage : ContentPage
+{
+	public ToDoPage()
+	{
+		InitializeComponent();
+	}
+
+    private void OnAddTaskClicked(object sender, EventArgs e)
+    {
+
+    }
+}
